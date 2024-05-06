e8423d7830d95d071be088fa5bed6586c971a125
Vip0938 <75055638+Vip0938@users.noreply.github.com>
2024-05-06 15:10:22 -0400

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -36,12 +36,12 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
-              name: 'Support Server',
+              name: 'No support, srry',
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
               url: 'https://discord.gg/xQF9f9yUEM'
           })
-            .setDescription(`No support for you lol`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setDescription(`No support for you, here's a random picture`)
+            .setImage('https://newvision-media.s3.amazonaws.com/cms/4040184f-4412-4775-980f-d2df40139bd3.jpg')
             .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
